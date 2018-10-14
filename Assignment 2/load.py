3f19f58057055d56f2c8b4a7d01f284996d0273f
Junjie Zeng <jjzworkstation@gmail.com>
2018-10-14 17:40:51 +1300

    Update load.py
    Update load.py to only have train-test split for three dataset

--- Assignment 2/load.py
+++ Assignment 2/load.py
@@ -1,109 +1,58 @@
 
 import numpy as np
-from timeit import default_timer as timer
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split, StratifiedKFold, RepeatedStratifiedKFold
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
-from sklearn.dummy import DummyClassifier
-from sklearn.linear_model import Lasso, SGDClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import accuracy_score
-
+from sklearn.model_selection import train_test_split, StratifiedKFold
 import pandas as pd
 
-random_seed = 5432363
+# use course number as random_seed
+random_seed = 755
 
 def writeData(src, train, test):
     train.reset_index().to_csv(src + '/train.csv', index=False)
     test.reset_index().to_csv(src + '/test.csv', index=False)
 
-# World Cup 2018 Data Preparation
-dat1 = pd.read_csv("data/World_Cup_2018/2018 worldcup.csv")
-dat1.Total_Scores.describe()
-dat1['IsNormalTime'] = 1
-dat1.loc[dat1.Normal_Time == 'No', 'IsNormalTime'] = 0
-dat1['indices'] = 5
-dat1.loc[dat1.Match_ID < 61, 'indices'] = 4
-dat1.loc[dat1.Match_ID < 57, 'indices'] = 3
-dat1.loc[dat1.Match_ID < 49, 'indices'] = 2
-dat1.loc[dat1.Match_ID < 33, 'indices'] = 1
-dat1.loc[dat1.Match_ID < 17, 'indices'] = 0
-dat1.indices.value_counts() # world cup match cycle
-dat1.drop(columns=['Match_result', 'Team1_Continent', 'Team2_Continent', 'Normal_Time','Phase', 'Location', 'Date'], inplace=True) # Phase, Location and Continent can perform further aggregation
-
-match_result = dat1[['IsNormalTime','Total_Scores','indices']]
-match_result.columns = ['IsNormalTime','Scores','indices']
-cols_name = ['Team', 'Attempts', 'Corners', 'Offsides', 
-        'Ball_Possession(%)', 'Pass_Accuracy(%)', 'Distance_Covered', 
-        'Ball_Recovered', 'Yellow_Card', 'Red_Card', 'Team1_Fouls']
-team1 = dat1.loc[:, dat1.columns[1:12]]
-team1.columns = cols_name
-team1_result = pd.concat([match_result, team1],axis=1)
-team2 = dat1.loc[:, dat1.columns[12:23]]
-team2.columns = cols_name
-team2_result = pd.concat([match_result, team2],axis=1)
-all_team = pd.concat([team1_result, team2_result], axis=0)
-all_team.reset_index(inplace=True, drop=True)
-all_team.sort_values(['Team', 'indices'],ascending=True, inplace=True)
-all_team.reset_index(inplace=True, drop=True)
-all_team.to_csv("data/World_Cup_2018/statistics.csv", index=False)
-schedule = dat1[['Team1', 'Team2','Total_Scores','indices']]
-test = schedule.query('indices >= 5').reset_index(drop=True)
-train = schedule.query('indices < 5').reset_index(drop=True)
-writeData('data/World_Cup_2018', train, test)
 
 # Traffic Congestion Data Preparation
 dat2 = pd.read_csv("data/Traffic_flow/traffic_flow_data.csv")
 train, test = train_test_split(dat2, test_size=0.1, random_state=random_seed)
 writeData('data/Traffic_flow', train, test)
 
+train['Segment23_(t+1)'].describe()
+#count    6750.000000
+#mean      301.378222
+#std       198.898209
+#min         0.000000
+#25%        93.000000
+#50%       327.000000
+#75%       482.000000
+#max      1350.000000
+#Name: Segment23_(t+1), dtype: float64
+test['Segment23_(t+1)'].describe()
+#count    750.000000
+#mean     313.038667
+#std      195.452943
+#min        0.000000
+#25%      120.000000
+#50%      359.500000
+#75%      485.000000
+#max      815.000000
+#Name: Segment23_(t+1), dtype: float64
+
 # Occupancy Sensor Data Preparation
 dat3 = pd.read_csv("data/Occupancy_sensor/occupancy_sensor_data.csv")
-dat3.Occupancy.value_counts() # classification
-dat3.sort_values('date', inplace=True)
-dat3.reset_index(inplace=True)
-# parse datetime
-dat3.date = pd.to_datetime(dat3.date)
+train, test = train_test_split(dat3, test_size=0.1, 
+                               random_state=random_seed,
+                               stratify = dat3['Occupancy'])
+writeData('data/Occupancy_sensor', train, test)
 
-# extract day
-dat3['day'] = dat3.date.dt.date
+train.Occupancy.value_counts()
+#0    12120
+#1     3288
+#Name: Occupancy, dtype: int64
 
-# group by rank
-dat3['id'] = dat3.groupby('day')['index'].rank(ascending=True)
-
-dat3.drop(['date', 'index'],axis=1,inplace=True)
-
-# base data
-base = dat3[['day', 'id', 'Occupancy']]
-measurement = dat3.copy()
-measurement.drop('Occupancy', axis=1, inplace=True) # ensure measurement has no occupancy information
-measurement.sort_values(['day', 'id'], inplace=True)
-Ks = [0, 1, 3, 5, 7, 9, 11]
-for index, k in enumerate(Ks):
-    temp1 = measurement.copy() 
-    temp1['id'] = temp1['id'] - k
-    temp1 = temp1.query('id > 0') # temp1 is before
-    temp2 = measurement.merge(temp1[['day', 'id']], on=['day', 'id'], how='inner') # temp2 is current
-    temp1.reset_index(inplace=True, drop=True)
-    temp2.reset_index(inplace=True, drop=True)
-    difft = temp1.iloc[:,0:5] - temp2.iloc[:, 0:5]
-    difft.columns = ['diff_' + item for item in difft.columns]
-    temp3 = temp1[['day', 'id']]
-    temp4 = pd.concat([temp3, difft],axis=1)
-    temp4['id'] = temp4['id'] + k
-    dat3_new = dat3.merge(temp4, how='left', on=['day', 'id'])
-    dat3_new = dat3_new.fillna(0)
-    train, test = train_test_split(dat3_new, test_size=0.1, random_state=random_seed)
-    train.reset_index().to_csv('data/Occupancy_sensor/train_%s.csv' % index, index=False)
-    test.reset_index().to_csv('data/Occupancy_sensor/test_%s.csv' % index, index=False)
+test.Occupancy.value_counts()
+#0    1347
+#1     365
+#Name: Occupancy, dtype: int64
 
 
 # LandSat Data Preparation
@@ -122,6 +71,24 @@ location2 = [item+"_"+ str(num) for item in location for num in [1,2,3,4]]
 dat4 = pd.read_csv("data/Landsat/lantsat.csv", names=location2+["label"])
 dat4.label.value_counts() # classification
 dat4.iloc[:,16:20] # core
-train, test = train_test_split(dat4, test_size=0.1, stratify = dat4.label, random_state=random_seed)
+train, test = train_test_split(dat4, test_size=0.1, stratify = dat4.label, 
+                               random_state=random_seed)
 writeData('data/Landsat', train, test)
 
+train.label.value_counts()
+#7    1344
+#1    1153
+#3    1145
+#2     619
+#5     580
+#4     559
+#Name: label, dtype: int64
+
+test.label.value_counts()
+#7    149
+#1    128
+#3    127
+#2     69
+#5     65
+#4     62
+#Name: label, dtype: int64
